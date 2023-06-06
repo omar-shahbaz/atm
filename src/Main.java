028d5518ea207dc28de3b028990a1e9ed2d78e3d
Omar Shahbaz <omar.khokhar@ericsson.com>
2023-06-06 15:41:28 +0500

    ATM-16

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         
         Transaction transaction = new TransactionImpl();
+        boolean check = true;
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Username: ");
@@ -20,20 +21,34 @@ public class Main {
         if(UserAuth.USERNAME.equals(currentUser) && UserAuth.PIN.equals(currentPin)){
             System.out.println("Welcome! "+currentUser);
             do {
-                switch (option.nextInt()) {
-                    case 1:
-                        transaction.withdraw(option.nextInt());
-                        break;
-                    case 2:
-                        transaction.deposit(option.nextInt());
-                        break;
-                    case 3:
-                        transaction.checkBalance();
-                        break;
-                    default:
-                        System.out.println("Invalid operation!");
+                System.out.println("Choose action to perform: ");
+                System.out.println("1. Withdraw Money");
+                System.out.println("2. Deposit Money");
+                System.out.println("3. Check Balance");
+                System.out.print("Choose: ");
+                int choice = option.nextInt();
+                do {
+                    switch (choice) {
+                        case 1:
+                            System.out.print("Amount to Withdraw: ");
+                            transaction.withdraw(option.nextInt());
 
-                }
+                            break;
+                        case 2:
+                            System.out.print("Amount to deposit");
+                            transaction.deposit(option.nextInt());
+
+                            break;
+                        case 3:
+                            System.out.print("Check balance: ");
+                            transaction.checkBalance();
+
+                            break;
+                        default:
+                            System.out.println("Invalid operation!");
+                    }
+                    check=false;
+                }while(check);
             }while (true);
         }
         else {
