c3c96c8b4d674de7ce23f64929d27dbc5ff5f7ec
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-06-17 15:36:58 +0500

    ATM-28 Added branch code in transaction

--- src/payload/TransactionMenu.java
+++ src/payload/TransactionMenu.java
@@ -6,7 +6,7 @@ public class TransactionMenu {
     private String num;
 
     public TransactionMenu(){
-        transactionInfo[0] = "MBS Transactions";
+        transactionInfo[0] = "MBS Transactions (1024)";
         transactionInfo[1] = "Withdraw";
         transactionInfo[2] = "Deposit";
         transactionInfo[3] = "Check Balance";
@@ -14,14 +14,14 @@ public class TransactionMenu {
     }
 
     public String[] createTransactionWindow(){
-        String infoAlignment = "| %-2s | %-18s|%n";
-        System.out.format("+------------------------+%n");
+        String infoAlignment = "| %-2s | %-24s|%n";
+        System.out.format("+------------------------------+%n");
         System.out.format(infoAlignment, num, transactionInfo[0]);
-        System.out.format("+------------------------+%n");
+        System.out.format("+------------------------------+%n");
         for(int i=1;i<4;i++) {
             System.out.format(infoAlignment,i,transactionInfo[i]);
         }
-        System.out.format("+------------------------+%n");
+        System.out.format("+------------------------------+%n");
 
 
 
